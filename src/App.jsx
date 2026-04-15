1646f76f476328346a23b65c98f84863b7f19195
CanataProgrammer <kaitoex86@gmail.com>
2026-04-15 16:09:28 +0900

    color fix

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ function App() {
   return (
     <div className="app-container">
       <header className="main-header">
-        <h1 className='bigtitle'>PDF Linker</h1>
+        <h1><font color="black">PDF Linker</font></h1>
         {user ? (
           <div className="user-info">
             <img src={user.photoURL} alt="icon" className="user-icon" />
